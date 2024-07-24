869c502dc42c3d5ecec5deeec02232cab0b2406e
VismayaVS-cpu <135596604+VismayaVS-cpu@users.noreply.github.com>
2024-07-24 17:54:12 +0530

    Add files via upload

--- Selenium/Activity2.py
+++ Selenium/Activity2.py
@@ -1,16 +1,32 @@
+# Set up the Firefox Driver with WebDriverManger
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+service = FirefoxService(GeckoDriverManager().install())
 
-with webdriver.Chrome() as driver:
-
+# Start the Driver
+with webdriver.Firefox(service=service) as driver:
+    # Navigate to the URL
     driver.get("https://v1.training-support.net/selenium/login-form")
+    # Print the title of the page
+    print("Page title is: ", driver.title)
 
-    print(f"Title of the page is {driver.title}")
+    # Find the username field
+    username = driver.find_element(By.ID, "username")
+    # Find the password field
+    password = driver.find_element(By.ID, "password")
 
-    driver.find_element(By.ID, "username").send_keys("admin")
-    driver.find_element(By.ID, "password").send_keys("password")
-    driver.find_element(By.XPATH, "//*[text()='Log in']").click()
-    print(f"Title of the page is {driver.title}")
-    
+    # Enter the given credentials
+    # Enter username
+    username.send_keys("admin")
+    # Enter password
+    password.send_keys("password")
 
-    driver.quit()
+    # Find the login button
+    login = driver.find_element(By.CSS_SELECTOR, "button.button:nth-child(4)")
+    login.click()
+
+    # Print the login message
+    message = driver.find_element(By.ID, "action-confirmation")
+    print("Login message: ", message.text)
